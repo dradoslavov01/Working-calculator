b5d88110bce3ef349f76b8b19eed421cf85c13c8
dradoslavov01 <daniradoslavov01@abv.bg>
2020-12-06 17:40:59 +0200

    Added calculator app

---
+++ Calculator/calculator.js
@@ -0,0 +1,16 @@
+function func(num){
+    document.form.textview.value += num; 
+}
+function back(){
+    var exp = document.form.textview.value;
+    if(exp){
+        document.form.textview.value = exp.substring(0,exp.length-1);
+    }
+}
+function clean(){
+    document.form.textview.value = ""; 
+}
+function equal(){
+    let a = document.form.textview.value;
+    document.form.textview.value = eval(a);
+}
